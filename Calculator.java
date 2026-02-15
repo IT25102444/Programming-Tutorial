a688492550cd8ec6fda814ec5bbace7fb2c748be
IT25102444 <it25102444@my.sliit.lk>
2026-02-15 12:54:09 +0530

    Sem2 Maths Tutorial done
    IT25102444 - Weediyage D.W.A.S

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+class Calculator {
+
+    // add() – adds two integers
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // multiply() – multiplies two integers
+    int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // square() – returns square of a number
+    int square(int a) {
+        return a * a;
+    }
+}
